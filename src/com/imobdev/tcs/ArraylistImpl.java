5f79b419950dfaed43499cc7b1f4bb6f983fdddd
Umesh <up9158256325@gmail.com>
2023-04-28 11:18:28 +0530

    Arraylist and HashMap Class Added

---
+++ src/com/imobdev/tcs/ArraylistImpl.java
@@ -0,0 +1,21 @@
+package com.imobdev.tcs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ArraylistImpl {
+	
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<>();
+		
+		list.add("Dilip");
+		list.add("Rekha");
+		list.add("rajesh");
+		list.add("Ashwni");
+		list.add("Umesh");
+		
+		System.out.println(list);
+	}
+
+}
---
+++ src/com/imobdev/tcs/HashMapDemo.java
@@ -0,0 +1,8 @@
+package com.imobdev.tcs;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Umesh Dilip Patil");
+	}
+}
